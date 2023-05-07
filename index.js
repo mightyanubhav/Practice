811f660a59361a24234ae9e0885257530886c10d
Anubhav Shukla <kumaranubhav691@gmail.com>
2023-05-08 02:59:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const  countValue = document.querySelector('#counter');
+
+const increment= ()=>{
+    let value = parseInt(countValue.innerText);
+    value++;
+    countValue.innerText = value;
+};
+const decrement= () =>{
+    let value = parseInt(countValue.innerText);
+    value--;
+    countValue.innerText = value;
+};
