8a8f44e0fcb8486493238580d57d2efa65468ef4
Anubhav Shukla <kumaranubhav691@gmail.com>
2023-05-10 01:27:39 +0530

    firstcommit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-header'>
+        This is america
+      </div>
+    </div>
+  );
+}
+
+export default App;
