170cf8fe125d14240c387ec0323e27014fa99e04
Anubhav Shukla <kumaranubhav691@gmail.com>
2023-05-10 23:53:00 +0530

    ndw

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-
+import Video from './components/Video';
 function App() {
   return (
     <div className="App">
       <div className='App-header'>
-        This is america
+        This is Hindustan
+        <Video/>
       </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ my-react-app/src/components/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Video() {
+  return (
+   <>
+     <img src='https://loremflickr.com/300/180
+' alt=''></img>
+   </>
+  )
+}
+
+export default Video
